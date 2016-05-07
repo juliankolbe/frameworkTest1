f98631cdcf3642f5289fe045b0121bcbf3abb2ff
Julian Kolbe <juliankolbe@live.com>
2016-05-07 16:30:33 +0200

    added Auryn injector and implemented it with http component

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -21,8 +21,13 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
-$response = new \Http\HttpResponse;
+//$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+//$response = new \Http\HttpResponse;
+
+$injector = include('Dependencies.php');
+
+$request = $injector->make('Http\HttpRequest');
+$response = $injector->make('Http\HttpResponse');
 
 
 foreach ($response->getHeaders() as $header) {
@@ -54,7 +59,7 @@ switch ($routeInfo[0]) {
         $method = $routeInfo[1][1];
         $vars = $routeInfo[2];
 
-        $class = new $className($response);
+        $class = $injector->make($className);
         $class->$method($vars);
         break;
 }
