9439f211731d4399cf53ef050cfd00cd549c1639
Julian Kolbe <juliankolbe@live.com>
2016-05-07 15:32:49 +0200

    added http component

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,40 @@
 <?php
 
-echo 'Hello World!';
+namespace FrameworkTest;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+ * Register the error handler
+ */
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
+$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new \Http\HttpResponse;
+
+
+foreach ($response->getHeaders() as $header) {
+    header($header, false);
+}
+
+
+
+//$content = '<h1>Hello World</h1>';
+//$response->setContent($content);
+
+$response->setContent('404 - Page not found');
+$response->setStatusCode(404);
+
+echo $response->getContent();
